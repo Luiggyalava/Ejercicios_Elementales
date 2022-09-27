0637a89346d1180e9da5648b544bfdaa70fd50a6
Luiggyalava <steven.alavavavivanco@tiernoparla.es>
2022-09-27 20:09:37 +0200

    ejercicio terminado print pares v1 - for por pares

--- src/main/java/com/tiernoparla/ejercicios_elementales/Ejercicios_elementales.java
+++ src/main/java/com/tiernoparla/ejercicios_elementales/Ejercicios_elementales.java
@@ -3,6 +3,17 @@ package com.tiernoparla.ejercicios_elementales;
 
 public class Ejercicios_elementales {
     public static void main(String[] args) {
-        System.out.println("Hola pintate");
+        
+        //pintar numeros del numero 1 al 10 usamos for variables y bucles
+        for (int i = 2; i < 11; i = i + 2){
+            System.out.println(i);
+        }//cierro for
+        
+        
+        
+        
+        
+        
+        
     } //fin main
 } // fin Ejercicios_elementales
