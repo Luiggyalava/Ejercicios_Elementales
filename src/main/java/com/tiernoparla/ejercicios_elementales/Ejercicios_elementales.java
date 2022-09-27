9f73f22c30a2789bd05baa56c2c2290d79a903ae
Luiggyalava <steven.alavavavivanco@tiernoparla.es>
2022-09-27 18:50:15 +0200

    se añade java

---
+++ src/main/java/com/tiernoparla/ejercicios_elementales/Ejercicios_elementales.java
@@ -0,0 +1,8 @@
+
+package com.tiernoparla.ejercicios_elementales;
+
+public class Ejercicios_elementales {
+    public static void main(String[] args) {
+        System.out.println("Hola pintate");
+    } //fin main
+} // fin Ejercicios_elementales
